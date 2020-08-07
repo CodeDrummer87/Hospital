fd3696824bbdf95fdeae9383225a029d5cb8e694
CodeDrummer <code.drummer87@gmail.com>
2020-08-07 18:47:06 +0600

    API controller with POST-method has been created.

---
+++ DoctorUI/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorUI.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Sex { get; set; }
+        public DateTime Birthday { get; set; }
+        public string AddressOfficial { get; set; }
+        public string AddressCurrent { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string Work { get; set; }
+        public string InsurancePolicy { get; set; }
+        public string RetirementInsurance { get; set; }       
+    }
+}
---
+++ DoctorUI/wwwroot/js/creatingPatient.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+
+	$('#createNewPatientButton').on('click', function () {
+		const patient = GetNewPatientData();
+		CreateNewPatient(patient);
+	});
+
+});
+
+function GetNewPatientData() {
+	return {
+		FirstName: $('#newPatientFirstName').val(),
+		LastName: $('#newPatientLastName').val(),
+		MiddleName: $('#newPatientMiddleName').val(),
+		Sex: $('input[name="patientSex"]:checked').val(),
+		Birthday: $('#newPatientBirthday').val(),
+		AddressCurrent: $('#newPatientAddressCurrent').val(),
+		AddressOfficial: $('#newPatientAddressOfficial').val(),
+		State: $('#newPatientState').val(),
+		Phone: $('#newPatientPhone').val(),
+		Work: $('#newPatientWork').val(),
+		InsurancePolicy: $('#newPatientInsurancePolicy').val(),
+		RetirementInsurance: $('#newPatientRetirementInsurance').val()
+	};
+}
+
+function CreateNewPatient(patient) {
+	if (patient !== null) {
+		$.ajax({
+			url: 'https://localhost:44349/api/patient/create',
+			method: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(patient),
+			success: function () {
+				alert("Попало куда нужно!");
+			},
+			error: function () {
+				alert("Ошибочка вышла!");
+			}
+		});
+	}
+}
